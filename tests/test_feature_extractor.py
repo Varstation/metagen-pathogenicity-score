8f27c115273150671c535e243efea4c1bf5765f1
Pedro Sebe <pedro.sebe@varsomics.com>
2022-02-21 18:59:14 -0300

    Adding assertions to tests

--- tests/test_feature_extractor.py
+++ tests/test_feature_extractor.py
@@ -10,25 +10,28 @@ class TestFeatureExtractor(unittest.TestCase):
     def test_missing_species(self):
         train_data = xr.DataArray(np.abs(np.random.randn(5,10)), {"Sample":range(5),"Species":list("abcdefghij")})
         test_data = xr.DataArray(np.abs(np.random.randn(10,5)), {"Sample":range(10,20),"Species":list("abcde")})
-        FeatureExtractor(3).fit(train_data).transform(test_data)
+        result = FeatureExtractor(3).fit(train_data).transform(test_data)
+        self.assertFalse(result.isnull().any())
     
     def test_new_species(self):
         train_data = xr.DataArray(np.abs(np.random.randn(5,10)), {"Sample":range(5),"Species":list("abcdefghij")})
         test_data = xr.DataArray(np.abs(np.random.randn(10,5)), {"Sample":range(10,20),"Species":list("ghijk")})
-        FeatureExtractor(3).fit(train_data).transform(test_data)
+        result = FeatureExtractor(3).fit(train_data).transform(test_data)
+        self.assertFalse(result.isnull().any())
     
     def test_new_species_only(self):
         train_data = xr.DataArray(np.abs(np.random.randn(5,10)), {"Sample":range(5),"Species":list("abcdefghij")})
         test_data = xr.DataArray(np.abs(np.random.randn(10,5)), {"Sample":range(10,20),"Species":list("klmno")})
-        FeatureExtractor(3).fit(train_data).transform(test_data)
+        result = FeatureExtractor(3).fit(train_data).transform(test_data)
+        self.assertFalse(result.isnull().any())
     
     def test_fit_transform_result_adds_up(self):
         sim_data = xr.DataArray(np.abs(np.random.randn(5,10)), {"Sample":range(5),"Species":list("abcdefghij")})
         reconstruction = FeatureExtractor(10).fit_transform(sim_data)
-        self.assertTrue(np.allclose(reconstruction.sum("Feature").unstack("Instance").to_pandas(), sim_data))
+        self.assertTrue(np.allclose(reconstruction.sum("Feature").to_pandas(), sim_data))
     
     def test_fit_result_adds_up(self):
         train_data = xr.DataArray(np.abs(np.random.randn(5,10)), {"Sample":range(5),"Species":list("abcdefghij")})
         test_data = xr.DataArray(np.abs(np.random.randn(10,5)), {"Sample":range(10,20),"Species":list("ghijk")})
         reconstruction = FeatureExtractor(10).fit(train_data).transform(test_data)
-        self.assertTrue(np.allclose(reconstruction.sum("Feature").unstack("Instance").to_pandas(), test_data))
+        self.assertTrue(np.allclose(reconstruction.sum("Feature").to_pandas(), test_data))
