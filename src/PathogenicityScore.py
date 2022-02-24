242e45cb488e57f1fbec5adec18b3e59928da849
Pedro Sebe <pedro.sebe@varsomics.com>
2022-02-24 12:57:51 -0300

    adding predict/predict_proba methods + tests

--- src/PathogenicityScore.py
+++ src/PathogenicityScore.py
@@ -1,6 +1,8 @@
 import xarray as xr
 from sklearn.decomposition import NMF
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import RobustScaler
+from sklearn.pipeline import make_pipeline
 import pandas as pd
 
 from ete3 import NCBITaxa
@@ -13,7 +15,7 @@ class FeatureExtractor:
     
     def fit(self, X):
         for coord in X.coords:
-            if coord != "Sample":
+            if coord != "sample":
                 taxon = coord
         self.seen_taxa = X.coords[taxon].values
 
@@ -26,20 +28,20 @@ class FeatureExtractor:
     
     def transform(self, X):
         for coord in X.coords:
-            if coord != "Sample":
+            if coord != "sample":
                 taxon = coord
 
         W = xr.DataArray(
             self.nmf.transform(X.reindex({taxon:self.seen_taxa}, fill_value=0).values),
-            {"Sample":X.coords["Sample"].values, "feature":range(1,self.nmf.n_components+1)},
-            ["Sample", "feature"])
+            {"sample":X.coords["sample"].values, "feature":range(1,self.nmf.n_components+1)},
+            ["sample", "feature"])
         residuals = X - W @ self.components_
         
         # Organisms absent from the training data have to be considered separately.
         # Since they are not part of the learned components, all their abundance has
         # to be allocated to residuals.
         missing_cols = set(X.coords[taxon].values) - set(self.seen_taxa)
-        residuals = xr.concat([residuals, X.sel(species=list(missing_cols))], taxon)
+        residuals = xr.concat([residuals, X.sel({taxon:list(missing_cols)})], taxon)
         residuals = residuals.assign_coords(feature="residual")
         
         features = (
@@ -51,15 +53,15 @@ class FeatureExtractor:
     
     def fit_transform(self, X):
         for coord in X.coords:
-            if coord != "Sample":
+            if coord != "sample":
                 taxon = coord
         self.seen_taxa = X.coords[taxon].values
 
         self.nmf.fit(X.values)
         W = xr.DataArray(
             self.nmf.fit_transform(X.values),
-            {"Sample":X.coords["Sample"].values, "feature":range(1,self.nmf.n_components+1)},
-            ["Sample", "feature"])
+            {"sample":X.coords["sample"].values, "feature":range(1,self.nmf.n_components+1)},
+            ["sample", "feature"])
         self.components_ = xr.DataArray(
             self.nmf.components_,
             {"feature":range(1,self.nmf.n_components+1), taxon:X.coords[taxon].values},
@@ -71,7 +73,7 @@ class FeatureExtractor:
         return features
 
 class PathogenicityClassifier:
-    def __init__(self, clf=LogisticRegression, taxonomic_info=None, **kwargs):
+    def __init__(self, clf=make_pipeline(RobustScaler(), LogisticRegression()), taxonomic_info=None, **kwargs):
         self.clf = clf(**kwargs_starting_with("clf_", kwargs))
         self.taxonomic_info = taxonomic_info
         self.kwargs = kwargs
@@ -86,7 +88,7 @@ class PathogenicityClassifier:
                 raise ValueError(f"Rank {rank} was specified, but no '{rank}_abundance' variable was not found in data")
 
     def fit(self, data):
-        self.taxonomy_levels = set(data.coords) - {"Sample"}
+        self.taxonomy_levels = set(data.coords) - {"sample"}
         self.input_validation(data)
         # - Generate NMF features for each taxonomy level using FeatureExtractor
         # - Put all xarray.DataArray's in the dict NMF_features
@@ -101,13 +103,39 @@ class PathogenicityClassifier:
                 self.NMF_features_[rank] = self.NMF_features_[rank].loc[{rank:self.taxonomic_info[rank]}].reset_coords(drop=True)
         
         # Train supervised algorithm with extracted features
-        self.predictors_ = xr.concat(self.NMF_features_.values(), dim="feature").stack(instance=("species","Sample")).to_pandas().T
-        self.target_ = data["classification"].stack(instance=("species","Sample")).to_pandas()
+        observed_species = data["species_abundance"].stack(instance=("species","sample")).to_pandas()>0
+        self.predictors_ = xr.concat(self.NMF_features_.values(), dim="feature").stack(instance=("species","sample")).to_pandas().T
+        self.target_ = data["classification"].stack(instance=("species","sample")).to_pandas()
         if (self.predictors_.index != self.target_.index).any():
             raise ValueError("Predictor matrix and target matrix should have equal indices")
-        self.clf.fit(self.predictors_, self.target_)
+        self.predictors_, self.target_ = self.predictors_.loc[observed_species], self.target_.loc[observed_species]
+        self.clf_ = self.clf.fit(self.predictors_, self.target_)
 
         return self
+    
+    def predict_proba(self, data):
+        self.taxonomy_levels = set(data.coords) - {"sample"}
+        self.input_validation(data)
+        
+        self.NMF_features_ = {}
+        for rank in self.taxonomy_levels:
+            feature_names = [f"{rank}_{i+1}" for i in range(self.NMF_[rank].n_components)] + [f"{rank}_residual"]
+            self.NMF_features_[rank] = self.NMF_[rank].transform(data[f"{rank}_abundance"]).assign_coords(feature=feature_names)
+            if rank != "species":
+                self.NMF_features_[rank] = self.NMF_features_[rank].loc[{rank:self.taxonomic_info[rank]}].reset_coords(drop=True)
+        
+        # Evaluate supervised algorithm with extracted features
+        observed_species = data["species_abundance"].stack(instance=("species","sample")).to_pandas()>0
+        self.predictors_ = xr.concat(self.NMF_features_.values(), dim="feature").stack(instance=("species","sample")).to_pandas().T
+        self.predictors_ = self.predictors_.loc[observed_species]
+        prob = xr.DataArray(
+            self.clf_.predict_proba(self.predictors_),
+            {"class":self.clf_.classes_, "instance":self.predictors_.index},
+            ["instance", "class"])
+        return prob
+
+    def predict(self, data):
+        return self.predict_proba(data).argmax("class")
 
 def get_rank(species, rank):
     for tx, rk in ncbi.get_rank(ncbi.get_lineage(species)).items():
@@ -119,10 +147,11 @@ def make_xarray(abundance_tables, classification):
     if "species" not in abundance_tables.keys():
         raise ValueError("make_xarray needs a species-level abundance table (tables for other taxonomic levels are optional)")
     taxonomic_info = {rank:xr.DataArray(abundance_tables["species"].columns.map(lambda x: get_rank(x, rank)), {"species":abundance_tables["species"].columns}) for rank, table in abundance_tables.items() if rank!="species"}
-    abundance_tables = {f"{rank}_abundance":xr.DataArray(table, {"Sample":table.index, rank:table.columns}) for rank, table in abundance_tables.items()}
-    return taxonomic_info, xr.Dataset(abundance_tables | dict(classification = xr.DataArray(classification, {"Sample":classification.index, "species":classification.columns})))
+    abundance_tables = {f"{rank}_abundance":xr.DataArray(table, {"sample":table.index, rank:table.columns}) for rank, table in abundance_tables.items()}
+    return taxonomic_info, xr.Dataset(abundance_tables | dict(classification = xr.DataArray(classification, {"sample":classification.index, "species":classification.columns})))
 
 def kwargs_starting_with(prefix, kwargs):
     # searches keyword arguments containing some prefix ending in '_', removes
     # the underscore and returns a dictionary with the results
     return {"_".join(key.split("_")[1:]):value for key, value in kwargs.items() if key.startswith(prefix)}
+
--- tests/test_feature_extractor.py
+++ tests/test_feature_extractor.py
@@ -8,30 +8,38 @@ if __name__ == "__name__":
 
 class TestFeatureExtractor(unittest.TestCase):
     def test_missing_species(self):
-        train_data = xr.DataArray(np.abs(np.random.randn(5,10)), {"Sample":range(5),"Species":list("abcdefghij")})
-        test_data = xr.DataArray(np.abs(np.random.randn(10,5)), {"Sample":range(10,20),"Species":list("abcde")})
+        train_data = xr.DataArray(np.abs(np.random.randn(5,10)), {"sample":range(5),"species":list("abcdefghij")})
+        test_data = xr.DataArray(np.abs(np.random.randn(10,5)), {"sample":range(10,20),"species":list("abcde")})
         result = FeatureExtractor(3).fit(train_data).transform(test_data)
         self.assertFalse(result.isnull().any())
     
     def test_new_species(self):
-        train_data = xr.DataArray(np.abs(np.random.randn(5,10)), {"Sample":range(5),"Species":list("abcdefghij")})
-        test_data = xr.DataArray(np.abs(np.random.randn(10,5)), {"Sample":range(10,20),"Species":list("ghijk")})
+        train_data = xr.DataArray(np.abs(np.random.randn(5,10)), {"sample":range(5),"species":list("abcdefghij")})
+        test_data = xr.DataArray(np.abs(np.random.randn(10,5)), {"sample":range(10,20),"species":list("ghijk")})
         result = FeatureExtractor(3).fit(train_data).transform(test_data)
         self.assertFalse(result.isnull().any())
     
     def test_new_species_only(self):
-        train_data = xr.DataArray(np.abs(np.random.randn(5,10)), {"Sample":range(5),"Species":list("abcdefghij")})
-        test_data = xr.DataArray(np.abs(np.random.randn(10,5)), {"Sample":range(10,20),"Species":list("klmno")})
+        train_data = xr.DataArray(np.abs(np.random.randn(5,10)), {"sample":range(5),"species":list("abcdefghij")})
+        test_data = xr.DataArray(np.abs(np.random.randn(10,5)), {"sample":range(10,20),"species":list("klmno")})
         result = FeatureExtractor(3).fit(train_data).transform(test_data)
         self.assertFalse(result.isnull().any())
     
     def test_fit_transform_result_adds_up(self):
-        sim_data = xr.DataArray(np.abs(np.random.randn(5,10)), {"Sample":range(5),"Species":list("abcdefghij")})
+        sim_data = xr.DataArray(np.abs(np.random.randn(5,10)), {"sample":range(5),"species":list("abcdefghij")})
         reconstruction = FeatureExtractor(10).fit_transform(sim_data)
-        self.assertTrue(np.allclose(reconstruction.sum("Feature").to_pandas(), sim_data))
+        self.assertTrue(np.allclose(reconstruction.sum("feature").to_pandas(), sim_data))
     
     def test_fit_result_adds_up(self):
-        train_data = xr.DataArray(np.abs(np.random.randn(5,10)), {"Sample":range(5),"Species":list("abcdefghij")})
-        test_data = xr.DataArray(np.abs(np.random.randn(10,5)), {"Sample":range(10,20),"Species":list("ghijk")})
+        train_data = xr.DataArray(np.abs(np.random.randn(5,10)), {"sample":range(5),"species":list("abcdefghij")})
+        test_data = xr.DataArray(np.abs(np.random.randn(10,5)), {"sample":range(10,20),"species":list("ghijk")})
         reconstruction = FeatureExtractor(10).fit(train_data).transform(test_data)
-        self.assertTrue(np.allclose(reconstruction.sum("Feature").to_pandas(), test_data))
+        self.assertTrue(np.allclose(reconstruction.sum("feature").to_pandas(), test_data))
+
+    def test_with_genus(self):
+        train_data = xr.DataArray(np.abs(np.random.randn(5,10)), {"sample":range(5),"genus":list("abcdefghij")})
+        test_data = xr.DataArray(np.abs(np.random.randn(10,5)), {"sample":range(10,20),"genus":list("ghijk")})
+        reconstruction = FeatureExtractor(10).fit(train_data).transform(test_data)
+        reconstruction_onestep = FeatureExtractor(10).fit_transform(test_data)
+        self.assertTrue(np.allclose(reconstruction.sum("feature").to_pandas(), test_data))
+        self.assertTrue(np.allclose(reconstruction_onestep.sum("feature").to_pandas(), test_data))
